159df40cb9987899cc910d2389ace03304f6b466
Zion0526 <129146794+Zion0526@users.noreply.github.com>
2023-05-15 16:21:58 +0900

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,20 @@ idCheckTemp = ''#아이디체크용 전역변수
 
 def AddLect(request):
     if request.method == 'AddLect':
-        pass
+        pass # 이 부분을 어떻게 써먹을지 몰라서 아래 새로 만들었어
+    def AddLect(request):
+        if request.method == 'POST':
+            professor = request.POST['professor']
+            lect_name = request.POST['lect_name']
+            user_id = request.user.user_id
+            user = User.objects.get(user_id=user_id)
+            lecture = Lecture(professor=professor, lectName=lect_name)
+            lecture.save()
+            user.lecture_set.add(lecture)
+            return redirect('MyPage')
+        else:
+            return render(request, 'AddLect.html')
+        # 강의 추가 기능(?) AddLect.html 템플릿에 추가 필요
 
 #회원가입 부분에서 사용할 함수들 
 
@@ -64,3 +77,21 @@ def Login(request):
 
 def Home(request):
     return render(request, 'accounts/home.html')
+
+# 마이페이지 함수
+def MyPage(request):
+    if request.method == 'POST': # 회원 정보 수정
+        user_id = request.POST['user_id'] # user_id 값을 가져옴
+        user = User.objects.get(user_id=user_id) 
+        user.name = request.POST['name']
+        user.age = request.POST['age']
+        user.email = request.POST['email'] # User 모델에서 user_id값에 해당하는 사용자 정보를 가져옴
+        user.save() # 사용자 정보를 사용자가 입력한 값으로 갱신
+        return redirect('MyPage') # 변경된 사용자 정보를 저장 후 MyPage로 리디렉션
+    else: # 메서드가 POST가 아닐 경우
+        user_id = request.user.user_id # 현재 로그인된 사용자의 user_id 값을 가져옴
+        user = User.objects.get(user_id=user_id)
+        lectures = user.lecture_set.all()
+        context = {'user': user, 'lectures' : lectures}
+        return render(request, 'MyPage.html', context)
+    # 사용자 정보를 MyPage.html 템플릿에 전달
