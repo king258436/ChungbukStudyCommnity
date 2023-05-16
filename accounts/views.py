faa67ef3d84f1d96dd579dab10c3e1108eae30f3
JaeChanHWANG <hss598876@gmail.com>
2023-05-16 15:09:04 +0900

    fix login and signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,94 +1,27 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
-from .models import User, Lecture
-from .loginfunc import *
+from .models import *
+from .forms import UserForm
 
-idCheckTemp = ''#아이디체크용 전역변수 
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password) 
+            # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'accounts/SignUp.html', {'form': form})
 
 #마이 페이지에서 사용할 함수 
 
-def AddLect(request):
-    if request.method == 'POST':
-        professor = request.POST['professor']
-        lect_name = request.POST['lect_name']
-        user_id = request.user.user_id
-        user = User.objects.get(user_id=user_id)
-        lecture = Lecture(professor=professor, lectName=lect_name)
-        lecture.save()
-        user.lecture_set.add(lecture)
-        return redirect('MyPage')
-    else:
-        return render(request, 'AddLect.html')
-        # 강의 추가 기능(?) AddLect.html 템플릿에 추가 필요
-
-#회원가입 부분에서 사용할 함수들 
-
-def CheckId(request):
-    global idCheckTemp
-    if request.method == 'CheckID':
-        id = request.CheckID['userId']
-        if CheckValidId(id):
-            idCheckTemp = id
-            #괜춘~
-        else:
-            pass
-            #안괜춘~
-
-def Register(request):
-    global idCheckTemp
-    if request.method == 'POST':
-        userInfo = User()
-        userInfo.age = request.POST['age']
-        userInfo.name = request.POST['name']
-        userInfo.user_id = request.POST['user_id']
-        userInfo.email = request.POST['email']
-        userInfo.user_pw = request.POST['user_pw']
-        tmp = Encryption(userInfo.user_pw)
-        userInfo.user_pw = tmp + (15 - len(tmp))*'0'
-        userInfo.save()
-        return render(request, 'accounts/login.html')
-    else:
-        return render(request, 'accounts/SignUp.html') 
-
-#로그인 부분에서 사용할 함수들 
-
-def Login(request):
-    if request.method == 'POST':
-        tmp_id = request.POST.get('myid')
-        tmp_pw = request.POST.get('mypw')
-        try:
-            checkObj = User.objects.get(user_id = tmp_id)
-            return render(request, '../main/index.html')
-            if checkObj.user_pw != Encryption(tmp_pw):
-                return render(request, 'accounts/login.html')
-            else:
-                return render(request, '../main/index.html')
-        except:
-            return render(request, 'accounts/login.html')
-        
-    else:
-        return render(request, 'accounts/login.html')# 안전하게 통과했으므로 home화면으로 넘어감 
-
-
 def Home(request):
     return render(request, 'accounts/home.html')
-
-# 마이페이지 함수
-def MyPage(request):
-    if request.method == 'POST': # 회원 정보 수정
-        user_id = request.POST['user_id'] # user_id 값을 가져옴
-        user = User.objects.get(user_id=user_id) 
-        user.name = request.POST['name']
-        user.age = request.POST['age']
-        user.email = request.POST['email'] # User 모델에서 user_id값에 해당하는 사용자 정보를 가져옴
-        user.save() # 사용자 정보를 사용자가 입력한 값으로 갱신
-        return redirect('MyPage') # 변경된 사용자 정보를 저장 후 MyPage로 리디렉션
-    else: # 메서드가 POST가 아닐 경우
-        user_id = request.user.user_id # 현재 로그인된 사용자의 user_id 값을 가져옴
-        user = User.objects.get(user_id=user_id)
-        lectures = user.lecture_set.all()
-        context = {'user': user, 'lectures' : lectures}
-        return render(request, 'MyPage.html', context)
-    # 사용자 정보를 MyPage.html 템플릿에 전달
     
 def Forget(request):
     return render(request, 'accounts/ps_change.html')
